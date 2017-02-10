ca4444c8978348dd71e833d55a1b6f4e5f569c89
bmacaw <mzmacaw@gmail.com>
2017-02-10 14:02:44 -0700

    Create basic article class with variables and get methods

--- app/src/main/java/com/example/android/newsapp/Article.java
+++ app/src/main/java/com/example/android/newsapp/Article.java
@@ -1,10 +1,45 @@
 package com.example.android.newsapp;
 
 /**
- * /**
- * An {@link Article} object contains information related to a single news article.
  * Created by Rebecca on 2/10/2017.
+ * An {@link Article} object contains information related to a single news article.
  */
 
 public class Article {
+
+    /**
+    * Web title of the article
+    */
+    private String mWebTitle;
+
+    /*
+    * Website URL of the article
+    */
+    private String mUrl;
+
+    /**
+    * Constructs a new (@link Article) object.
+    *
+    * @param title is the title of the article
+    * @param url is the website URL link to read the article
+    */
+    public Article(String mWebTitle, String mUrl) {
+        mWebTitle = title;
+        mUrl = url;
+    }
+
+    /**
+    * Returns the web title of the article
+    */
+    public String getmWebTitle() {
+        return mWebTitle;
+    }
+
+    /**
+     * Returns the website URL link to read the article
+     */
+    public String getmUrl() {
+        return mUrl;
+    }
+
 }
--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class NewsActivity extends AppCompatActivity {
 
+    /**
+     * URL for article data from the Guardian API dataset
+     */
+    private static final String GUARDIAN_REQUEST_URL = "http://content.guardianapis.com/environment?order-by=newest&api-key=d3787300-c41c-4d75-a322-5dd44c5bb024";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
